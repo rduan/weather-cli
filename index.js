54330e5061dad9b0f50ce32b9892bffdac418bed
Richard Duan <dxloader@gmail.com>
2018-05-03 23:32:02 +1000

    add minimist

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const minimist = require('minimist');
+
 module.exports = () => {
-  console.log('welcome to the outside');
-  
+  // console.log('welcome to the outside');
+  const args = minimist(process.argv.slice(2));
+  console.log(args);
 }
