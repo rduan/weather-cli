83c0987f5104b26a7f556c6bd04396fe005ece82
Richard Duan <dxloader@gmail.com>
2018-05-04 07:51:22 +1000

    get weather forecast

--- cmds/forecast.js
+++ cmds/forecast.js
@@ -1,4 +1,25 @@
-module.exports = (args) => {
-  console.log('tomorrow is rainy')
-  
+const ora = require('ora')
+const getWeather = require('../utils/weather-forecast')
+
+module.exports = async (args) => {
+  const spinner = ora().start()
+
+  try {
+    const location = args.location || args.l;
+    const weather = await getWeather(location);
+
+    spinner.stop();
+
+    // console.log(weather)
+    console.log(`Forecast for ${location}:`);
+    
+    weather.forecast.forEach(item => {
+      console.log(`\t${item.date} - Low: ${item.low}° | High: ${item.high}° | ${item.text}`);
+
+    });
+  } catch (err) {
+    spinner.stop();
+
+    console.error(err)
+  }
 }
